bb5a607a9dde32ba818ec29e4915978bf148bdde
Khearthik <karthikkomakula@gmail.com>
2020-10-22 18:37:34 -0400

    Simple pages first commit

---
+++ src/components/AboutPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function AboutPage() {
+  return (
+    <React.Fragment>
+      <h2> About</h2>
+      <p>This is a Paragraph</p>
+    </React.Fragment>
+  );
+}
+
+export default AboutPage;
---
+++ src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <div className="jumbotron">
+      <h1>Hello !!</h1>
+      <p>THis is the first Line</p>
+      <a href="/about">About</a>
+    </div>
+  );
+}
+
+export default HomePage;
